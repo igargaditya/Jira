09725409210b2a6f2d4dd21aeb009f3abc9ae03d
igargaditya <igargaditya@gmail.com>
2026-05-17 21:02:58 +0530

    Story entity changed

--- src/main/java/com/aditya/jira/entity/Story.java
+++ src/main/java/com/aditya/jira/entity/Story.java
@@ -3,16 +3,22 @@ package com.aditya.jira.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Getter
 @Setter
+@Document(collection = "stories")
 public class Story {
 
-    private Long id ;
+    @Id
+    private ObjectId id ;
     private String title ;
     private String description;
-    private Date createdAt ;
+    private LocalDateTime createdAt ;
 
 }
